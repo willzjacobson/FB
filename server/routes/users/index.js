1758b2bcf322e52dedbc153bf7c8c4b9604a7384
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2016-01-09 18:40:10 -0500

    draft of models

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,3 +1,11 @@
 var router = require('express').Router();
 module.exports = router;
 var mongoose = require('mongoose');
+
+// schema.methods.findWithSimilarHobbies = function() {
+// 	return this.find({ type: {"$in" : this.hobbies}})
+// 	.then(function(users) {
+
+// 	})
+// 	.then(null, next);
+// };
