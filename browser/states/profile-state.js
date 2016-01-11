4bfb9212e7549dc0c86b2b5d0dc7f57a6b04e4dd
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2016-01-11 12:59:49 -0500

    enough practice now

--- browser/states/profile-state.js
+++ browser/states/profile-state.js
@@ -8,12 +8,34 @@ app.config(function($stateProvider) {
 				return UserFactory.getOneUser($stateParams.userId);
 			}
 		}
+	})
+	.state('profile.friends', {
+		url: '/friends',
+		templateUrl: '/templates/profile.friends.html',
+		controller: function($scope) {
+			$scope.friends = $scope.user.desiredFriends;
+			
+			$scope.isFriend = function(userId) {
+				return ($scope.me.friends.indexOf(userId) >= 0 ||
+				$scope.me.desiredFriends.indexOf(userId) >= 0);
+			};
+			
+		},
+		resolve: {}
+	})
+	.state('profile.hobbies', {
+		url: '/hobbies',
+		templateUrl: '/templates/profile.hobbies.html',
+		controller: function($scope) {
+			$scope.hobbies = $scope.user.hobbies;
+		}
 	});
 });
 
 app.controller('ProfileCtrl', function($scope, user, AuthFactory) {
 
 	$scope.me = AuthFactory.getCurrentUser();
+	console.log('$scope.me', $scope.me);
 	$scope.user = user;
-console.log(user);
+
 });
